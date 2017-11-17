c7e512f1f861d2dad8ec17bf336989e7787c3744
tian <tian@tian.workgroup>
2017-11-17 23:35:16 +0800

    "[+]add readme"

--- a.js
+++
@@ -1,5 +0,0 @@
-console.log('run a');
-export default function() {
-    console.log('export a');
-}
-console.log('run over a');
--- b.js
+++
@@ -1,5 +0,0 @@
-import a from './a';
-console.log('run b ');
-export default function() {
-    console.log('export b');
-}
